e07b95e434f4af4155ec050a49a6b1e66b93d75e
Paulo <paulohfcmendes@gmail.com>
2019-08-04 19:51:20 -0300

    resumè finished

---
+++ data/data.js
@@ -0,0 +1,101 @@
+/*!
+ * Guim González - Portfolio v1.0.0 (https://guimgonzalez.me)
+ * Copyright 2019 Guim González
+ * Licensed under MIT (https://github.com/GuimG/dev-portfolio/master/LICENSE)
+ */
+
+// Change your profile data here
+let profile = {
+  picture: "https://avatars1.githubusercontent.com/u/26333000?v=4",
+  name: "Paulo Ferreira",
+  description: "Web & Mobile Developer.",
+  bio:
+    "I'm a network engineering student from Brasilia, working with web and mobile applications development as a freelancer.",
+  email: "paulohfcmendes@gmail.com",
+  location: "Brasília, Brasil",
+  //birth: "1997-07-26", // YYYY-MM-DD format
+  skills: [
+    "HTML",
+    "CSS",
+    "Javascript",
+    "Materialize",
+    "Bootstrap",
+    "PHP",
+    "ReactJS",
+    "React Native",
+    "ElectronJS",
+    "NodeJS",
+    "MongoDB",
+    "SQL",
+    "Git",
+    "Docker",
+    "Linux Server",
+    "Network Infrastructure"
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/paulo-henrique-ferreira-625b84171/",
+    github: "https://github.com/paulohlips/",
+    dev: "http://www.cerradoti.com.br/"
+  },
+  experience: [
+    {
+      title: "Mobile Developer",
+      company: "Universidade de Brasília - UnB",
+      //: "https://guimgonzalez.business/",
+      location: "Brasília, Brasil",
+      description:
+        "Development of a criminal expertise support application in a university partnership with the Brazilian Federal Police. On this occasion I worked as a mobile developer using React Native.",
+      from: "June 2018",
+      to: "July 2019"
+    },
+    {
+      title: "Backend Developer",
+      company: "CerradoTI",
+      url: "http://www.cerradoti.com.br/",
+      location: "Brasília, Brasil",
+      description: "Integration of APIs between suppliers and an Ecommerce.",
+      from: "October 2018",
+      to: "October 2018"
+    },
+    {
+      title: "Backend Developer",
+      company: "CerradoTI",
+      url: "http://www.cerradoti.com.br/",
+      location: "Brasília, Brasil",
+      description: "Development of e-commerce administrative panel.",
+      from: "October 2018",
+      to: "October 2018"
+    }
+  ],
+  education: [
+    {
+      school: "Universidade de Brasília",
+      degree: "Engineering",
+      fieldOfStudy: "Communication Networks",
+      location: "Brasília, Brasil",
+      from: "March 2016",
+      to: "Now"
+    },
+    {
+      school: "Rocketseat",
+      degree: "Mobile Developer",
+      fieldOfStudy: "React Native",
+      location: "Brasília, Brasil",
+      from: "November 2018",
+      to: "March 2019"
+    }
+  ],
+  quote: "Turning code into solutions."
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
